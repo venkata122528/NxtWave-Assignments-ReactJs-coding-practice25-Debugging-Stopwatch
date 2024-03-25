f32a8eae2dfdff9deec90bc53151a13464a8d24b
venkata122528 <venkata122528@gmail.com>
2024-03-25 12:49:59 +0000

    debugging stopwatch

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-named-as-default */
+// eslint-disable-next-line import/no-named-as-default-member
+import Stopwatch from './components/Stopwatch'
+
+import './App.css'
+
+const App = () => <Stopwatch />
+
+export default App
